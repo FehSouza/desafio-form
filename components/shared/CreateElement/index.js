87a1c98336ca3b4349b8d4bed07f2fb8a6870d0c
Fernanda <engcivil.fernandasouza@gmail.com>
2022-03-03 18:28:24 -0300

    alteracao da estrutura do CreateElement

--- components/shared/CreateElement/index.js
+++ components/shared/CreateElement/index.js
@@ -1,52 +1,33 @@
+const addClass = (elem, value) => elem.classList.add(value);
+const addClasses = (elem, classes) => elem.classList.add(...classes);
+const addChild = (elem, child) => elem.appendChild(child);
+const addChildren = (elem, children) => children.forEach((item) => addChild(elem, item));
+const addTextContent = (elem, text) => (elem.textContent = text);
+const onClick = (elem, evt) => elem.addEventListener('click', evt);
+const mouseOver = (elem, evt) => elem.addEventListener('mouseover', evt);
+const mouseOut = (elem, evt) => elem.addEventListener('mouseout', evt);
+const keyUp = (elem, evt) => elem.addEventListener('keyup', evt);
+const onChance = (elem, evt) => elem.addEventListener('change', evt);
+const addSetAttribute = (elem, key, value) => elem.setAttribute(key, value);
+
 export const CreateElement = (type, props) => {
   const elem = document.createElement(type);
 
-  for (const key in props) {
-    if (key === 'class' && !Array.isArray(props[key])) {
-      elem.classList.add(props[key]);
-      continue;
-    }
+  if (props === undefined || props === null) return elem;
 
-    if (key === 'class' && Array.isArray(props[key])) {
-      elem.classList.add(...props[key]);
-      continue;
-    }
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === 'class' && !Array.isArray(value)) return addClass(elem, value);
+    if (key === 'class' && Array.isArray(value)) return addClasses(elem, value);
+    if (key === 'children' && !Array.isArray(value)) return addChild(elem, value);
+    if (key === 'children' && Array.isArray(value)) return addChildren(elem, value);
+    if (key.toLowerCase() === 'textcontent') return addTextContent(elem, value);
+    if (key.toLowerCase() === 'onclick') return onClick(elem, value);
+    if (key.toLowerCase() === 'mouseover') return mouseOver(elem, value);
+    if (key.toLowerCase() === 'mouseout') return mouseOut(elem, value);
+    if (key.toLowerCase() === 'keyup') return keyUp(elem, value);
+    if (key.toLowerCase() === 'onchange') return onChance(elem, value);
+    return addSetAttribute(elem, key, value);
+  });
 
-    if (key === 'children' && !Array.isArray(props[key])) {
-      if (props[key] === undefined || props[key] === null) continue;
-      elem.appendChild(props[key]);
-      continue;
-    }
-
-    if (key === 'children' && Array.isArray(props[key])) {
-      for (const item of props[key]) {
-        if (item === undefined || item === null) continue;
-        elem.appendChild(item);
-      }
-      continue;
-    }
-
-    if (key.toLowerCase() === 'textcontent') {
-      elem.textContent = props[key];
-      continue;
-    }
-
-    if (key.toLowerCase() === 'onclick') {
-      elem.addEventListener('click', props[key]);
-      continue;
-    }
-
-    if (key.toLowerCase() === 'onchange') {
-      elem.addEventListener('change', props[key]);
-      continue;
-    }
-
-    if (key.toLowerCase() === 'keyup') {
-      elem.addEventListener('keyup', props[key]);
-      continue;
-    }
-
-    elem.setAttribute(key, props[key]);
-  }
   return elem;
 };
